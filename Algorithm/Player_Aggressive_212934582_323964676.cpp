4f23d67e3b0ac5af0fc2ebb6f15aeb0a21c42539
almirafarah <almirafarah@mail.tau.ac.il>
2025-08-25 12:54:03 +0300

    mm

--- Algorithm/Player_Aggressive_212934582_323964676.cpp
+++ Algorithm/Player_Aggressive_212934582_323964676.cpp
@@ -1,5 +1,6 @@
 #include "Player_Aggressive_212934582_323964676.h"
 #include "../common/BattleInfo.h"
+#include "../UserCommon/MyBattleInfo_212934582_323964676.h"
 #include "../common/PlayerRegistration.h"
 
 namespace Algorithm_212934582_323964676 {
@@ -9,14 +10,36 @@ Player_Aggressive_212934582_323964676::Player_Aggressive_212934582_323964676(int
 }
 
 void Player_Aggressive_212934582_323964676::updateTankWithBattleInfo(TankAlgorithm& tank_algo, SatelliteView& satellite_view) {
-    (void)satellite_view;
-    // Create a battle info object for aggressive player
-    BattleInfo info;
-    
-    // Update the tank algorithm with battle info
+    using UserCommon_212934582_323964676::MyBattleInfo;
+
+    // Determine map dimensions by probing the satellite view
+    size_t width = 0;
+    while (satellite_view.getObjectAt(width, 0) != '&') {
+        ++width;
+    }
+    size_t height = 0;
+    while (satellite_view.getObjectAt(0, height) != '&') {
+        ++height;
+    }
+
+    // Reconstruct board and locate our tank (marked by '%')
+    std::vector<std::string> board(height, std::string(width, ' '));
+    size_t tank_row = 0, tank_col = 0;
+    for (size_t y = 0; y < height; ++y) {
+        for (size_t x = 0; x < width; ++x) {
+            char c = satellite_view.getObjectAt(x, y);
+            if (c == '%') {
+                tank_row = y;
+                tank_col = x;
+                c = static_cast<char>('0' + player_index_);
+            }
+            board[y][x] = c;
+        }
+    }
+
+    // Build battle info with basic data; orientation and shells are not tracked
+    MyBattleInfo info(height, width, board, tank_row, tank_col, 0, num_shells_);
     tank_algo.updateBattleInfo(info);
-    
-    // Aggressive player - focused on offensive tactics
 }
 
 } // namespace Algorithm_212934582_323964676
--- Algorithm/Player_Simple_212934582_323964676.cpp
+++ Algorithm/Player_Simple_212934582_323964676.cpp
@@ -1,5 +1,6 @@
 #include "Player_Simple_212934582_323964676.h"
 #include "../common/BattleInfo.h"
+#include "../UserCommon/MyBattleInfo_212934582_323964676.h"
 #include "../common/PlayerRegistration.h"
 
 namespace Algorithm_212934582_323964676 {
@@ -9,14 +10,33 @@ Player_Simple_212934582_323964676::Player_Simple_212934582_323964676(int player_
 }
 
 void Player_Simple_212934582_323964676::updateTankWithBattleInfo(TankAlgorithm& tank_algo, SatelliteView& satellite_view) {
-    (void)satellite_view;
-    // Create a battle info object for simple/defensive player
-    BattleInfo info;
-    
-    // Update the tank algorithm with battle info
+    using UserCommon_212934582_323964676::MyBattleInfo;
+
+    size_t width = 0;
+    while (satellite_view.getObjectAt(width, 0) != '&') {
+        ++width;
+    }
+    size_t height = 0;
+    while (satellite_view.getObjectAt(0, height) != '&') {
+        ++height;
+    }
+
+    std::vector<std::string> board(height, std::string(width, ' '));
+    size_t tank_row = 0, tank_col = 0;
+    for (size_t y = 0; y < height; ++y) {
+        for (size_t x = 0; x < width; ++x) {
+            char c = satellite_view.getObjectAt(x, y);
+            if (c == '%') {
+                tank_row = y;
+                tank_col = x;
+                c = static_cast<char>('0' + player_index_);
+            }
+            board[y][x] = c;
+        }
+    }
+
+    MyBattleInfo info(height, width, board, tank_row, tank_col, 0, num_shells_);
     tank_algo.updateBattleInfo(info);
-    
-    // Simple player - focused on defensive/evasive tactics
 }
 
 } // namespace Algorithm_212934582_323964676
